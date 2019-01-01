ceda6623ea904c55e94cb0590fdb6cc34200232c
Thomas Kunkel <thomas.boyd.kunkel@gmail.com>
2019-01-01 21:24:01 +0000

    Automatically backed up by Learn

--- fizzbuzz.rb
+++ fizzbuzz.rb
@@ -4,9 +4,9 @@ def fizzbuzz(num)
   if num % 3 == 0 && num % 5 == 0
     return "FizzBuzz"
   elsif num % 5 == 0
-    return "Fizz"
-  elsif num % 3 == 0
     return "Buzz"
+  elsif num % 3 == 0
+    return "Fizz"
   else
     return nil
   end
